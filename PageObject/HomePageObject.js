b3529b7f79e69b3b789ab9e262bd1d643f9c8b45
Omer Mukhtar Khan <omukhtar@NIS-PK566-MacBook-Pro.local>
2022-02-02 17:46:21 +0500

    added test case for homepage banner navigation

--- PageObject/HomePageObject.js
+++ PageObject/HomePageObject.js
@@ -16,16 +16,17 @@ myElements = {
                         selector : '//div[@class="category-cards"]/div[@class="card mt-4 top-card"][3]//h5',
                         locateStrategy: 'xpath'
                  },
-                 widgetsHomePage : 
+        widgetsHomePage : 
                  {
                         selector : '//div[@class="category-cards"]/div[@class="card mt-4 top-card"][4]//h5',
                         locateStrategy: 'xpath'
-                 }
+                 },
+        bannerHomePage: 'img[class="banner-image"]'
 };
 
 myCommands = [
         {
-            clickOnHomePageAndVerify(input){
+            async clickOnHomePageAndVerify(input){
                     if(input == "elements")
                     {
                         return this.click('@elementHomePage').pause(3000).assert.urlEquals('https://demoqa.com/elements');
@@ -42,6 +43,26 @@ myCommands = [
                     {
                         return this.click('@widgetsHomePage').pause(3000).assert.urlEquals('https://demoqa.com/widgets');
                     }
+                    else if(input == "bannerHomePage")
+                    {
+                       this.click('@bannerHomePage',function(){
+                            this.windowHandles(function(result){
+                                console.log(result.value);
+                                result = result.value[1];
+                                this.switchWindow(result,function(){
+                                    this.waitForElementVisible("body",30000);
+                                    this.assert.title("Tools QA - Selenium Training");
+                                });
+
+                            });
+
+
+
+                       });
+                       
+                    }
+
+                    
                     
             }
 
@@ -57,24 +78,3 @@ commands:myCommands,
 url: "https://demoqa.com"
 
 };
-
-
-
-
-
-// if(input == "elements")
-//                     {
-//                         return this.click('@elementHomePage').pause(3000).assert.urlEquals('https://demoqa.com/elements');
-//                     }
-//                     else if(input == "forms")
-//                     {
-//                         return this.click('@formHomePage').pause(3000).assert.urlEquals('https://demoqa.com/forms');
-//                     }
-//                     else if(input == "alerts")
-//                     {
-//                         return this.click('@alertsHomePage').pause(3000).assert.urlEquals('https://demoqa.com/alertsWindows');
-//                     }
-//                     else if(input == "widgets")
-//                     {
-//                         return this.click('@widgetsHomePage').pause(3000).assert.urlEquals('https://demoqa.com/widgets');
-//                     }
--- tests/HomePage.js
+++ tests/HomePage.js
@@ -10,44 +10,51 @@ module.exports = {
        browser.end();
    },
 
-"verifying elements on home page":(browser)=>{
+// "verifying elements on home page":(browser)=>{
         
-        const homePageElementsExpected = ["Elements","Forms","Alerts, Frame & Windows","Widgets","Interactions","Book Store Application"];
+//         const homePageElementsExpected = ["Elements","Forms","Alerts, Frame & Windows","Widgets","Interactions","Book Store Application"];
 
-        browser.getText('div[class="category-cards"]', (result)=>{
+//         browser.getText('div[class="category-cards"]', (result)=>{
 
-            let homePageElementsActual = result.value.split("\n");
-            console.log(homePageElementsActual);
+//             let homePageElementsActual = result.value.split("\n");
+//             console.log(homePageElementsActual);
 
-            for(let i=0;i<homePageElementsExpected.length;i++)
+//             for(let i=0;i<homePageElementsExpected.length;i++)
 
-            {
-            browser.assert.equal(homePageElementsActual.includes(homePageElementsExpected[i]),true,"Expected is"+homePageElementsExpected[i]);
-            }
-        })
-}
-,
+//             {
+//             browser.assert.equal(homePageElementsActual.includes(homePageElementsExpected[i]),true,"Expected is"+homePageElementsExpected[i]);
+//             }
+//         })
+// }
+// ,
 
-"verifying redirection after clicking on Elements on home page'":(browser)=>{
-            const homePage1 = browser.page.HomePageObject();
-            homePage1.clickOnHomePageAndVerify("elements");
-}
-,
+// "verifying redirection after clicking on Elements on home page'":(browser)=>{
+//             const homePage1 = browser.page.HomePageObject();
+//             homePage1.clickOnHomePageAndVerify("elements");
+// }
+// ,
 
-"verifying redirection after clicking on Forms on home page'":(browser)=>{
-    const homePage2 = browser.page.HomePageObject();
-    homePage2.clickOnHomePageAndVerify("forms");
-}
-,
+// "verifying redirection after clicking on Forms on home page'":(browser)=>{
+//     const homePage2 = browser.page.HomePageObject();
+//     homePage2.clickOnHomePageAndVerify("forms");
+// }
+// ,
 
-"verifying redirection after clicking on Alerts, Frame & Windows on home page'":(browser)=>{
-    const homePage3 = browser.page.HomePageObject();
-    homePage3.clickOnHomePageAndVerify("alerts");
-},
+// "verifying redirection after clicking on Alerts, Frame & Windows on home page'":(browser)=>{
+//     const homePage3 = browser.page.HomePageObject();
+//     homePage3.clickOnHomePageAndVerify("alerts");
+// },
 
-"verifying redirection after clicking on Widgets on home page'":(browser)=>{
-    const homePage4 = browser.page.HomePageObject();
-    homePage4.clickOnHomePageAndVerify("widgets");
+// "verifying redirection after clicking on Widgets on home page'":(browser)=>{
+//     const homePage4 = browser.page.HomePageObject();
+//     homePage4.clickOnHomePageAndVerify("widgets");
+// },
+
+
+
+"verifying new tab open when we click on home page banner'":(browser)=>{
+    const homePage5 = browser.page.HomePageObject();
+    var result = homePage5.clickOnHomePageAndVerify("bannerHomePage");
 }
 
 };
